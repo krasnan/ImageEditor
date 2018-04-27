764fa8f0594a37cca28fa7091fbb1ddd3b7922c7
Martin Krasňan <martin.krasnan@gmail.com>
2018-04-27 02:41:53 +0200

    create new file and set filename when empty

--- specials/SpecialImageEditor.php
+++ specials/SpecialImageEditor.php
@@ -6,18 +6,21 @@
  * @ingroup Extensions
  */
 
-class SpecialImageEditor extends SpecialPage {
-	public function __construct() {
-		parent::__construct( 'ImageEditor' );
-	}
+class SpecialImageEditor extends SpecialPage
+{
+    public function __construct()
+    {
+        parent::__construct('ImageEditor');
+    }
 
-	/**
-	 * Show the page to the user
-	 *
-	 * @param string $sub The subpage string argument (if any).
-	 *  [[Special:HelloWorld/subpage]].
-	 */
-	public function execute( $sub ) {
+    /**
+     * Show the page to the user
+     *
+     * @param string $sub The subpage string argument (if any).
+     *  [[Special:HelloWorld/subpage]].
+     */
+    public function execute($sub)
+    {
         $user = $this->getUser();
         $rights = $user->getRights();
         global $wgGroupPermissions, $wgRevokePermissions;
@@ -25,17 +28,55 @@ class SpecialImageEditor extends SpecialPage {
         $out = $this->getOutput();
 
 
-        if(!$this->getUser()->isAllowed('edit')){
+        if (!$this->getUser()->isAllowed('edit')) {
             $out->showPermissionsErrorPage([['badaccess-group0']], 'edit');
-        }
-        else{
+        } else if ( !isset($_GET["file"])) {
+            $this->renderFileNameForm();
+        } else {
             $template = new ImageEditorTemplate();
             $template->setRef("context", $this);
             $out->addModules(["ext.imageEditor"]);
             $out->addTemplate($template);
         }
     }
-    function getGroupName() {
+
+    function getGroupName()
+    {
         return 'media';
+    }
+
+    private function renderFileNameForm()
+    {
+        $this->getOutput()->setPageTitle($this->msg('ie-create-image'));
+        $this->getOutput()->addWikiText($this->msg('ie-create-image-description'));
+
+        $formDescriptor = [
+            'file' => [
+                'placeholder' => 'ExampleName',
+                'maxlength' => 100,
+                'required' => true,
+                'vertical-label'=>true,
+                'class' => 'HTMLTextField',
+                'label' => $this->msg('ie-insert-image-name')
+            ]
+        ];
+        $htmlForm = new HTMLForm($formDescriptor, $this->getContext(), 'testform');
+
+        $htmlForm->setSubmitText($this->msg('ie-create-with-image-editor'));
+        $htmlForm->setSubmitCallback([$this, 'checkFileName']);
+
+        $htmlForm->show();
+    }
+
+    public function checkFileName($formData)
+    {
+        global $wgScript, $wgMessage;
+        preg_match('/^[a-zá-žA-ZÁ-Ž0-9_]{1,100}$/', $formData["file"], $matches, PREG_OFFSET_CAPTURE);
+
+        if(count($matches) === 0)
+            return $this->msg('ie-invalid-image-name');
+        $file = "File:". ucfirst($formData["file"]).".png";
+        $this->getOutput()->redirect($wgScript . '?title=Special:ImageEditor&file=' . $file . "&returnto=" . $file);
+        return true;
     }
 }
